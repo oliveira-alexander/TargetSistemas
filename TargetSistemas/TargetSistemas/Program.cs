d0a924518454ed5d8ea30d4dbc3620b9fc6349f6
oliveira-alexander <oliveir4.4lexander@gmail.com>
2025-05-10 08:14:47 -0300

    Criação do projeto

---
+++ TargetSistemas/TargetSistemas/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TargetSistemas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
